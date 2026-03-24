d9c1df7d894acde608822da42095a582a07f33ce
EduardoAguilar92 <55557870+EduardoAguilar92@users.noreply.github.com>
2026-03-24 15:26:20 -0600

    Initial commit

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar">
+    <div class="logo">Mi App</div>
+    <ul class="nav-links">
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Servicios</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+  </nav>
+</template>
